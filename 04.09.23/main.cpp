adf8c5272b22309ee782bb11e00d4c9091790368
f1reEnd <104725621+f1reEnd@users.noreply.github.com>
2023-09-11 09:27:28 +0300

    Example

---
+++ 04.09.23/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // std::cout, std::endl
+
+int main()
+{
+    int a;
+    std::cout << 1+1+1+1<< std::endl;
+    std::cin >> a;
+    std::cout << a << std::endl;
+    return 0;
+}
